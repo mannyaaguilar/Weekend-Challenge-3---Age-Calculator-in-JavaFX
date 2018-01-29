514aaeae53ea46a43509010ae63552ea4208caa6
Manny Aguilar <mannyaaguilar@yahoo.com>
2018-01-29 12:14:22 -0700

    Updated the code from Sunday night

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,64 +6,99 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
-    @FXML
-    private TextField entryField;
 
     @FXML
-    private Text questionText;
+    public Text responseText;
 
     @FXML
-    private Text responseText;
+    public Text responseText2;
 
     @FXML
-    private Text responseText2;
+    public Text responseText3;
 
     @FXML
-    private Text responseText3;
+    public Text responseText4;
+
 
     @FXML
-    private Button btnSubmit;
-
-    String name = "none";
-    String age = null;
-    String year = "xxxx";
+    public Text greetingText;
 
 
-        public void onSubmitClick(ActionEvent actionEvent){
+    @FXML
+    public TextField userInput;
 
-            String userInput = entryField.getText();
+    @FXML
+    public Button clickyButton;
 
-            if(name.equals("none")){
-            name = userInput;
-            btnSubmit.getText();
-            entryField.clear();}
+    @FXML
+    public Text questionText;
 
-            else if(year.equals("xxxx")){
-            year = userInput;
-            questionText.setText("Please enter current year:");
-            btnSubmit.getText();
-            entryField.clear();}
+    public int whatClick = 0;
 
-            else if(age.equals(null)){
-            age = userInput;
-            questionText.setText("Please enter your age:");
-            btnSubmit.getText();
-            entryField.clear();
+    public String userName = "";
+    public String userAge = "";
+    public String userYear = "";
 
-            int age = Integer.parseInt(userInput);
+    String[] questions = {
+            "What is your name:",
+            "What is your age:",
+            "What is the current year:"
+    };
 
-            int followingYear = age + 1;
-            int ageInFiveYears = age + 5;
-            int ageInTenYears = age + 10;
+    public void clickyButton(ActionEvent actionEvent) {
+        whatClick++;
 
+        if (whatClick == 1) {
+            userName = userInput.getText();
+            questionText.setText(questions[1]);
+            userInput.clear();
+        } else if (whatClick == 2) {
+            userAge = userInput.getText();
+            questionText.setText(questions[2]);
+            userInput.clear();
+        } else if (whatClick == 3) {
+            userYear = userInput.getText();
+            userInput.clear();
 
-            responseText.setText("You will be " + followingYear + " next year.");
-            responseText2.setText("You will be " + ageInFiveYears + " in five years.");
-            responseText3.setText("You will be " + ageInTenYears + " in ten years.");}
+            int currentAge = Integer.parseInt(userAge);
+            int currentYear = Integer.parseInt(userYear);
+
+            int followingYear = currentAge + 1;
+            int ageInFiveYears = currentAge + 5;
+            int ageInTenYears = currentAge + 10;
+
+            int nextYear = currentYear + 1;
+            int inFiveYears = currentYear + 5;
+            int inTenYears = currentYear + 10;
+
+            responseText.setText(userName + ", you are currently " + userAge + " years old.");
+            responseText2.setText("In 2019, you will be " + followingYear + " years old.");
+            responseText3.setText("In 2023, you will be " + ageInFiveYears + " years old.");
+            responseText4.setText("In 2028, you will be " + ageInTenYears + " years old.");
+
+            clickyButton.setText("Reset");
+            userInput.setDisable(true);
+        } else if (whatClick == 4) {
+            clickyButton.setText("Submit");
+            whatClick = 0;
+            questionText.setText(questions[0]);
+            userInput.setDisable(false);
+            responseText.setText(" ");
+            responseText2.setText(" ");
+            responseText3.setText(" ");
+            responseText4.setText(" ");
         }
 
     }
+}
+
+
+
+
 
 
